0f1731aa927ebb998acf00297c9f72158f8c4885
Ahd <dafuq.ahd@gmail.com>
2020-06-27 08:05:37 +0300

    add form to header

--- front-page.php
+++ front-page.php
@@ -43,6 +43,88 @@
         </ul>
       </div>
     </nav>
+    <div class="front_page-header-content">
+      <h1>My Turkey Property</h1>
+      <form action="<?php echo the_permalink();?>/properties" method="get">
+        <div class="input-group">
+          <label>Location:</label>
+          <select name="location" class="form-control form-control-sm">
+            <option value="">Any</option>
+            <option value="antalia" <?php selected( isset($_GET['location']) ? $_GET['location'] : '', 'antalia' ); ?>>
+              Antalia</option>
+            <option value="bursa" <?php selected( isset($_GET['location']) ? $_GET['location'] : '', 'bursa' ); ?>>Bursa
+            </option>
+            <option value="fethiye" <?php selected( isset($_GET['location']) ? $_GET['location'] : '', 'fethiye' ); ?>>
+              Fethiye
+            </option>
+            <option value="istanbul"
+              <?php selected( isset($_GET['location']) ? $_GET['location'] : '', 'istanbul' ); ?>>
+              Istanbul</option>
+          </select>
+        </div>
+
+        <div class="input-group">
+          <label>Type:</label>
+          <select name="type" class="form-control form-control-sm">
+            <option value="">Any</option>
+            <option value="apartment" <?php selected( isset($_GET['type']) ? $_GET['type'] : '', 'apartment' ); ?>>
+              Apartment
+            </option>
+            <option value="penthouse" <?php selected( isset($_GET['type']) ? $_GET['type'] : '', 'penthouse' ); ?>>
+              Penthouse
+            </option>
+            <option value="villa" <?php selected( isset($_GET['type']) ? $_GET['type'] : '', 'villa' ); ?>>
+              Villa
+            </option>
+          </select>
+        </div>
+        <div class="input-group">
+          <label>MIN. Bedrooms:</label>
+          <select name="minbedrooms" class="form-control form-control-sm">
+            <option value="">Any</option>
+            <option value="1" <?php selected( isset($_GET['minbedrooms']) ? $_GET['minbedrooms'] : '', '1' ); ?>>1
+            </option>
+            <option value="2" <?php selected( isset($_GET['minbedrooms']) ? $_GET['minbedrooms'] : '', '2' ); ?>>2
+            </option>
+            <option value="3" <?php selected( isset($_GET['minbedrooms']) ? $_GET['minbedrooms'] : '', '3' ); ?>>3
+            </option>
+            <option value="4" <?php selected( isset($_GET['minbedrooms']) ? $_GET['minbedrooms'] : '', '4' ); ?>>4
+            </option>
+            <option value="5" <?php selected( isset($_GET['minbedrooms']) ? $_GET['minbedrooms'] : '', '5' ); ?>>5
+            </option>
+          </select>
+        </div>
+        <div class="input-group">
+          <label>MIN. Bathrooms:</label>
+          <select name="minbathrooms" class="form-control form-control-sm">
+            <option value="">Any</option>
+            <option value="1" <?php selected( isset($_GET['minbathrooms']) ? $_GET['minbathrooms'] : '', '1' ); ?>>1
+            </option>
+            <option value="2" <?php selected( isset($_GET['minbathrooms']) ? $_GET['minbathrooms'] : '', '2' ); ?>>2
+            </option>
+            <option value="3" <?php selected( isset($_GET['minbathrooms']) ? $_GET['minbathrooms'] : '', '3' ); ?>>3
+            </option>
+          </select>
+        </div>
+        <div class="input-group">
+          <label>Min Price:</label>
+          <input class="form-control form-control-sm" type="number" name="minprice" placeholder="Min Price" />
+        </div>
+        <div class="input-group">
+          <label>Max Price:</label>
+          <input class="form-control form-control-sm" type="number" name="maxprice" placeholder="Max Price" />
+        </div>
+        <div class="input-group">
+          <label>REF NO.:</label>
+          <input placeholder="e.g. ista1" name="refno" value="<?php echo $_GET['refno']?>"
+            class="form-control form-control-sm" />
+        </div>
+        <div class="input-group">
+          <button class="btn btn-primary btn-sm" type="submit" name="">Search</button>
+        </div>
+      </form>
+    </div>
   </header>
   <div style="height: 100vh"></div>
+
   <?php get_footer();?>
